539e723f529b4ea8d8d40faf01523ad22eb86bfa
Shaxzodbek55 <torakulovshaxzod67@gmail.com>
2025-01-21 21:08:38 +0500

    Add files via upload

---
+++ scrippt.js
@@ -0,0 +1,39 @@
+// let num = (2 + 2)
+// alert{num}  num
+
+// alert(9234561786483920) BigInt
+
+
+// let str= 'MARS'
+// let str2= "MARS"
+// let str3 = `MARS${str1}`  String
+
+
+// let num = 4 < 1
+// alert(num)   Boolean 
+
+
+// null  //hatolik
+
+// let sum;
+
+// alert(sum);   underfined
+
+
+let user = prompt (`strim typdagi malumotni kiriting`);
+console.log(`${user}.`);
+
+let age = prompt (`number typ dagi malumotnikiriting`);
+console.log(`Yosh: ${age}`);
+
+let fan = prompt (`boolen typdagi malumotni kiriting`);
+console.log(`Fan: ${fan}`);
+
+let add = prompt (`yashaydigan tumaningizni kiriting`);
+console.log(`Fan: ${add}`);
+
+let thank = ('hamma malumotlarni togri kiritganingizga ishonchingiz komilmi');
+alert(thank)
+
+
+
